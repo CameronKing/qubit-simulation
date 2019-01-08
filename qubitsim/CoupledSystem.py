5986b9fa1a13a84598eaa18652fde7081440ed54
Cameron King <ceking5@wisc.edu>
2019-01-08 15:44:01 -0600

    Starting some bare init and methods prototyping

--- qubitsim/CoupledSystem.py
+++ qubitsim/CoupledSystem.py
@@ -1,8 +1,34 @@
 # A class to describe coupled qubit-resonator systems
 
 class CoupledSystem:
-    def __init__(self):
+    def __init__(self, qubit, resonator, coupling):
         """
-        Initializes a SOSS Hybrid qubit coupled to a resonator
+        Create an object consisting of a coupled qubit-resonator system
+
+        Parameters
+        ----------
+
+        qubit : Qubit object
+
+        resonator : resonator object
+
+        coupling : float
+          Units: GHz
+          Strength of the bare coupling
+        
+        Returns
+        -------
+        Coupled system object
         """
+
+        self.qubit = qubit
+        self.resonator = resonator
+        self.coupling = coupling
+        self.dim = qubit.dim + resonator.dim
+
+    def bareHamiltonian(self):
+        """
+        Return the bare Hamiltonian of the system
+        """
+        return None
         
