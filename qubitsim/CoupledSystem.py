5be439379928221a3fc1c9de2830c309b64737c3
Cameron King <ceking5@wisc.edu>
2019-01-08 15:39:39 -0600

    Started a class to simulate coupled systems

---
+++ qubitsim/CoupledSystem.py
@@ -0,0 +1,8 @@
+# A class to describe coupled qubit-resonator systems
+
+class CoupledSystem:
+    def __init__(self):
+        """
+        Initializes a SOSS Hybrid qubit coupled to a resonator
+        """
+        
